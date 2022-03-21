75437d3b45cc1f5615123ec5ab29ad0cfce91b38
Iqra-Jav <90656540+Iqra-Jav@users.noreply.github.com>
2022-03-21 16:48:29 +0000

    installed bootstrap

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by Iqra and is{" "}
-        <a href="#" target="_blank">
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+
+        <footer>
+          This project was coded by Iqra and is{" "}
+          <a href="#" target="_blank">
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
