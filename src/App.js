46bff05f378999b2b3967700d0a502c10ada3264
Iqra-Jav <90656540+Iqra-Jav@users.noreply.github.com>
2022-03-21 15:52:12 +0000

    created link to github repository

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by Iqra and is{" "}
+        <a href="#" target="_blank">
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
